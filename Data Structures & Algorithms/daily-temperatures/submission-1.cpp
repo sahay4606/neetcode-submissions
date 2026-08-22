7d01d2e20dd4fc863437f267b8ee51a1dcb42a85
Rishabh Sahay <f20240746@goa.bits-pilani.ac.in>
2026-08-22 16:26:55 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) 
+    // we not filling in order linearly , we fill the index in stack , use for and while inside , (compare the current one to all indexes left in stack , one we found for that we pop that element from stack , so we dont have do for it again , each new element is then pushed into the stack .. after the while loop , after the temp[i] > temp[st.top()] then that element in stack is resolved so no need.. and we use ith temp to remove as many from the stack )
+    {
+        
+        stack <int> st ;
+        int n = temperatures.size();
+        vector <int> result(n,0);
+
+        for(int i = 0 ; i < n; i++ )
+        {
+            while(!st.empty() && temperatures[i] > temperatures[st.top()])
+            {
+                int prevd = st.top();
+                st.pop();
+
+                result[prevd]= i- prevd;
+            }
+
+            st.push(i);
+
+        }
+        return result;
+
+        
+    }
+};
